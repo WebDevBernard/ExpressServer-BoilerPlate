65c061b152851e3b6131783677786140a37e4ef9
Bernard Yang <ybernard@gmail.com>
2022-02-04 23:46:28 -0800

    complete exercise reverseint

--- JavaScript/algo-exercises/palindrome/index.js
+++ JavaScript/algo-exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// My solution
+// function palindrome(str) {
+//   if (str.split("").reverse().join("") === str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
+// solution 1
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("");
+
+//   return reversed === str;
+// }
+
+// solution 2
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 module.exports = palindrome;
--- JavaScript/algo-exercises/reverseint/index.js
+++ JavaScript/algo-exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// my solution
+// function reverseInt(n) {
+//   const solution = parseInt(n.toString().split("").reverse().join(""));
+//   if (n < 0) {
+//     return -solution;
+//   } else {
+//     return solution;
+//   }
+// }
+
+// solution 1
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+console.log(reverseInt(15));
